09b7a1a547836eed3f611782b283f452bf7fd69f
mert <mertishakilic@gmail.com>
2021-11-09 02:27:59 +0300

    Makes 10 game started

---
+++ Graduation Project/Assets/Makes 10/Script/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Makes10
+{
+    public class Ball : MonoBehaviour
+    {
+        public int number;
+
+        public GameObject glowCircle;
+        
+        public void GlowCirleSetActive(bool active)
+        {
+            glowCircle.SetActive(active);
+        }
+    }
+}    
+
+
---
+++ Graduation Project/Assets/Makes 10/Script/BallThrower.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+ 
+namespace Makes10
+{
+	public class BallThrower : MonoBehaviour
+	{
+		GameObject ballThrowersParent;
+
+		Transform ballThrower;
+
+		GameObject[] balls;
+
+		float timeCounter;
+
+		private void Start()
+		{
+			ballThrowersParent = GameObject.Find("BallThrowers");
+			balls = Resources.LoadAll<GameObject>("Makes 10/Balls");
+		}
+
+		private void Update()
+		{
+			timeCounter += Time.deltaTime;
+			if (timeCounter > 1)
+			{
+				ThrowBall();
+				timeCounter = 0;
+			}
+		}
+
+		void ThrowBall()
+		{
+			ballThrower = ballThrowersParent.transform.GetChild(Random.Range(0, 4));
+			if (ballThrower.transform.childCount <= 4)
+			{
+				Instantiate(balls[Random.Range(0, 9)], ballThrower);
+			}
+			else
+			{
+				Destroy(ballThrower.GetChild(0).gameObject);
+			}
+		}
+
+	}
+
+}
+
