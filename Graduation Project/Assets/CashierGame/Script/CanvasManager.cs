ba1a7c29e5fa26de9590c53e7079358cbb21bb48
mert <mertishakilic@gmail.com>
2021-11-09 19:11:50 +0300

    Cashier game started

---
+++ Graduation Project/Assets/CashierGame/Script/CanvasManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace CashierGame
+{
+    public class CanvasManager : MonoBehaviour
+    {
+        public Text[] productPriceTexts;
+
+        public Text totalProductPriceText;
+        public Text customerMoneyText;
+        public Text changeText;
+
+        string[] productNames = { "ekmek", "kola", "ayran", "çiğköfte" };
+
+        public void SetTexts(List<float> productPrices, float totalProductPrice, float customerMoney, float change)
+        {
+            SetProductPricesToZero();
+            for (int i = 0; i < productPrices.Count; i++)
+            {
+                productPriceTexts[i].text = productNames[UnityEngine.Random.Range(0, productNames.Length)] + ": " + productPrices[i].ToString();
+            }
+            totalProductPriceText.text = "Total: " + totalProductPrice.ToString();
+            customerMoneyText.text = customerMoney.ToString();
+            changeText.text = change.ToString();
+        }
+
+        public void SetChangeText(float change)
+        {
+            changeText.text = change.ToString();
+        }
+
+        void SetProductPricesToZero()
+        {
+            for (int i = 0; i < productPriceTexts.Length; i++)
+            {
+                productPriceTexts[i].text = "";
+            }
+        }
+        
+    }
+} 
+
---
+++ Graduation Project/Assets/CashierGame/Script/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+namespace CashierGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        float totalProductPrice;
+        float customerMoney;
+        float change = 0;
+
+
+        private void Start()
+        {
+            CreateNewLevel();
+        }
+
+        void CreateNewLevel()
+        {
+            change = 0;
+            float tempTotalProductPrice = 0;
+            float tempCustomerMoney;
+
+            List<float> tempProductPrices = new List<float>();
+
+            int rand = UnityEngine.Random.Range(1, 4);
+            for (int i = 0; i < rand; i++)
+            {
+                tempProductPrices.Add(UnityEngine.Random.Range(1, 20));
+            }
+
+            for (int i = 0; i < tempProductPrices.Count; i++)
+            {
+                Debug.Log(tempProductPrices[i]);
+                tempTotalProductPrice += tempProductPrices[i];
+            }
+
+            tempCustomerMoney = UnityEngine.Random.Range(tempTotalProductPrice+.25f, tempTotalProductPrice * 2.2f);
+            tempCustomerMoney = Roll(tempCustomerMoney);
+            totalProductPrice = tempTotalProductPrice;
+            customerMoney = tempCustomerMoney;
+
+            GetComponent<CanvasManager>().SetTexts(tempProductPrices, totalProductPrice, customerMoney, change);
+        }
+
+        float Roll(float a)
+        {
+            // if given parameter is 88.71, it will return 88.50.
+            return System.Convert.ToSingle(System.Math.Round(a - (System.Math.Round(a, 2) % 0.25f), 2));
+        }
+
+        public void AddChanges(float a)
+        {
+            change += a;
+            GetComponent<CanvasManager>().SetChangeText(change);
+        }
+
+        public void Control()
+        {
+            if (totalProductPrice + change == customerMoney)
+            {
+                Invoke("CreateNewLevel", .75f);
+            }
+        }
+
+        public void DeleteChange()
+        {
+            change = 0;
+            GetComponent<CanvasManager>().SetChangeText(change);
+        }
+
+    }
+}
+
