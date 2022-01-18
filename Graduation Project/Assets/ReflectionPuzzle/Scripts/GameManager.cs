06bc7ff47b5631e2a86ca80277dc65d126492a3c
Enes <enes.durmus@stu.fsm.edu.tr>
2022-01-18 15:12:26 +0300

    ReflectionPuzzle Finished

--- Graduation Project/Assets/ReflectionPuzzle/Scripts/GameManager.cs
+++ Graduation Project/Assets/ReflectionPuzzle/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ReflectionPuzzle
 {
@@ -8,33 +9,127 @@ namespace ReflectionPuzzle
     {
         [SerializeField] private int rows = 3;
         [SerializeField] private int cols = 3;
-        [SerializeField] private float tileSize = 1;
-
-        [SerializeField] private float placementRateHorizontal;
-        [SerializeField] private float placementRateVertical;
 
         [SerializeField] private GameObject gridHolderReal;
         [SerializeField] private GameObject gridHolderReflection;
 
-        void Start()
+        [SerializeField] private Sprite[] shapes;
+
+        [SerializeField] private Text pointText;
+
+        private GridHolderController ghcReal;
+        private GridHolderController ghcReflected;
+
+        private int placementShapeNumber = 0;
+        private int[] randomTilesIndexes;
+        private bool isWrongShapeSelected = false;
+
+        private int point = 0;
+
+        private void Awake()
+        {
+            placementShapeNumber = (rows * cols) / 2;
+            ghcReal = gridHolderReal.GetComponent<GridHolderController>();
+            ghcReflected = gridHolderReflection.GetComponent<GridHolderController>();
+        }
+
+
+        private void Start()
         {
             CreateGridHolders();
         }
 
-        void Update()
-        {
-
-        }
-
         private void CreateGridHolders()
         {
-            gridHolderReal.GetComponent<GridManager>().CreateGridHolder(rows, cols, tileSize, placementRateHorizontal, placementRateVertical);
-            gridHolderReflection.GetComponent<GridManager>().CreateGridHolder(rows, cols, tileSize, 0.9f - placementRateHorizontal, placementRateVertical);
+            ghcReal.CreateTiles(rows, cols);
+            ghcReflected.CreateTiles(rows, cols);
+            PlaceShapes();
         }
 
         private void PlaceShapes()
         {
+            randomTilesIndexes = new int[placementShapeNumber];
+            int shapeCount = 0;
+            
+            for (int i = 0; i < placementShapeNumber; i++)
+            {
+                if (shapeCount == shapes.Length)
+                    shapeCount = 0;
 
+                randomTilesIndexes[i] = GetUniqueRandom();
+                ghcReal.CreateShape(randomTilesIndexes[i], shapes[shapeCount++]);         
+            }
+
+            PlaceReflectedShapes();
+        }
+
+        private void PlaceReflectedShapes()
+        {
+            int shapeCount = 0;
+            int index;
+
+            for (int i = 0; i < placementShapeNumber; i++)
+            {
+                if (shapeCount == shapes.Length)
+                    shapeCount = 0;
+
+                if (isWrongShapeSelected)
+                {
+                    index = randomTilesIndexes[i];
+                    ghcReflected.CreateShape(index, shapes[shapeCount++]);
+                }
+                else
+                {
+                    index = GetUniqueRandom();
+                    isWrongShapeSelected = true;
+                    ghcReflected.CreateShapeWrong(index, shapes[shapeCount++]);
+                }
+            }
+        }      
+
+        private int GetUniqueRandom()
+        {
+            int randomInt = Mathf.FloorToInt(Random.Range(0, rows * cols));
+            bool isUnique = false;
+
+            while (!isUnique)
+            {
+                if(CheckUniqueness(randomTilesIndexes, randomInt))
+                    return randomInt;
+                else
+                    randomInt = Mathf.FloorToInt(Random.Range(0, rows * cols));
+            }
+       
+            return 0;
+        }
+
+        private bool CheckUniqueness(int[] l, int number)
+        {
+            for (int i = 0; i < l.Length; i++)
+                if (randomTilesIndexes[i] == number)
+                    return false;
+
+            return true;
+        }
+
+        public void NextLevel()
+        {
+            ghcReflected.DestroyInstantiatedShapes();
+            ghcReal.DestroyInstantiatedShapes();
+
+            isWrongShapeSelected = false;
+
+
+            point += 10;
+            pointText.text = point.ToString();
+
+            PlaceShapes();
+        }
+
+        public void WrongSelected()
+        {
+            point -= 10;
+            pointText.text = point.ToString();
         }
     }
 }
