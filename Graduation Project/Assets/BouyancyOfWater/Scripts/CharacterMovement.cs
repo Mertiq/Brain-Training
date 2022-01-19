0de1d2bd877474c6b1c7f3f2bab946e7ae847466
Enes Kızılcin <nazifenes.kizilcin@stu.fsm.edu.tr>
2022-01-19 11:35:12 +0300

    uı , canvas refactoring

--- Graduation Project/Assets/BouyancyOfWater/Scripts/CharacterMovement.cs
+++ Graduation Project/Assets/BouyancyOfWater/Scripts/CharacterMovement.cs
@@ -7,49 +7,48 @@ namespace BouyancyOfWater{
 
     [RequireComponent(typeof(CharacterController2D))]
     public class CharacterMovement : MonoBehaviour
-{
-    private CharacterController2D controller;
+    {
+        private CharacterController2D controller;
 
-    //public GameObject gameOverPanel;
-    public float speed = 4.0f;
-    private bool isJump = false;
-    private float horizontalMovement = 0.0f; 
-    void Awake()
-    {
-        controller = GetComponent<CharacterController2D>();
-    }
+        public GameObject endGamePanel;
+        public float speed = 4.0f;
+        private bool isJump = false;
+        private float horizontalMovement = 0.0f; 
+        void Awake()
+        {
+            controller = GetComponent<CharacterController2D>();
+        }
 
-    void Update()
-    {
-         horizontalMovement = Input.GetAxisRaw("Horizontal") * speed;
-            Debug.Log(Input.GetAxisRaw("Horizontal"));
-         if(Input.GetButtonDown("Jump"))
-         {
-             isJump = true;
-         }
-    }
-  
-    void FixedUpdate()
-    {
-        controller.Move(horizontalMovement * Time.fixedDeltaTime,false, isJump);
-        isJump = false;
-    }
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if(col.transform.tag == "Wood")
+        void Update()
         {
-           
+            horizontalMovement = Input.GetAxisRaw("Horizontal") * speed;
+            if(Input.GetButtonDown("Jump"))
+            {
+                isJump = true;
+            }
         }
-        else if(col.transform.tag == "Water")
-        {
-            Destroy(gameObject);
-            Debug.Log("Game Over");
-        }
-        else if(col.transform.tag == "Finish Point")
-        {
-            //gameOverPanel.SetActive(true);
-        }
-    }
     
-}
+        void FixedUpdate()
+        {
+            controller.Move(horizontalMovement * Time.fixedDeltaTime,false, isJump);
+            isJump = false;
+        }
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if(col.transform.tag == "Wood")
+            {
+                //
+            }
+            else if(col.transform.tag == "Water")
+            {
+                Destroy(gameObject);
+                Debug.Log("Game Over");
+            }
+            else if(col.transform.tag == "FinishPoint")
+            {
+                endGamePanel.SetActive(true);
+            }
+        }
+        
+    }
 }
