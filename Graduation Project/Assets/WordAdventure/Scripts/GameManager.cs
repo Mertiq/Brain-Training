6907197a6801a733767937a83f8e020c5b240159
EnesDurmus <enesdurmus3738@gmail.com>
2022-04-15 17:37:55 +0300

    Car Controller, TouchController added

---
+++ Graduation Project/Assets/WordAdventure/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WordAdventure
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        private readonly string[] WordsList = { "Box", "Punch" };
+
+        [SerializeField] private TMPro.TextMeshProUGUI targetTextUi;
+
+        private string targetText;
+
+        void Start()
+        {
+            targetText = WordsList[GetRandomNum()];
+            targetTextUi.text = targetText;
+        }
+
+        private int GetRandomNum()
+        {
+            return Random.Range(0, WordsList.Length);
+        }
+    }
+}
