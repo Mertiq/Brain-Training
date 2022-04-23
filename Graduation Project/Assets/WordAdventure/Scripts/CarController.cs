9796c7b76d42473e2fc3777d154e17583e2e9a6c
EnesDurmus <enesdurmus3738@gmail.com>
2022-04-23 14:29:13 +0300

    Letter caught added

--- Graduation Project/Assets/WordAdventure/Scripts/CarController.cs
+++ Graduation Project/Assets/WordAdventure/Scripts/CarController.cs
@@ -7,13 +7,16 @@ namespace WordAdventure
 {
     public class CarController : MonoBehaviour
     {
+        string st = "BCDEOHPRSUV";
+
+        [SerializeField] private GameManager GameManager;
         [SerializeField] private GameObject RoadObject;
         [SerializeField] private GameObject CarObject;
+        [SerializeField] private GameObject Letters;
         [SerializeField] private float speed;
 
-        private Sequence turnSequence; 
+        private Sequence turnSequence;
         private int currentLane;
-
         void Start()
         {
             turnSequence = DOTween.Sequence();
@@ -33,9 +36,10 @@ namespace WordAdventure
                 takenWay += speed * Time.deltaTime;
                 elapsedTime += Time.deltaTime;
 
-                if (takenWay > 75f)
+                if (takenWay > 100f)
                 {
                     RoadObject.transform.Translate(0f, 0f, takenWay);
+                    Letters.transform.Translate(0f, 0f, takenWay);
                     takenWay = 0f;
                 }
 
@@ -69,10 +73,10 @@ namespace WordAdventure
             float pos = GetLanePosition();
 
             turnSequence.Append(transform.DORotate(new Vector3(0f, -8.45f, 0f), 0.15f).
-                OnComplete( () => transform.DORotate(new Vector3(0f, 4.25f, 0f), 0.3f)).
-                OnComplete( () => transform.DORotate(new Vector3(0f, 0f, 0f), 0.15f)));
-            turnSequence.Append(transform.DOMoveX(pos + 0.24f, 0.3f). 
-                OnComplete( () => transform.DOMoveX(pos, 0.3f)));
+                OnComplete(() => transform.DORotate(new Vector3(0f, 4.25f, 0f), 0.3f)).
+                OnComplete(() => transform.DORotate(new Vector3(0f, 0f, 0f), 0.15f)));
+            turnSequence.Append(transform.DOMoveX(pos + 0.24f, 0.3f).
+                OnComplete(() => transform.DOMoveX(pos, 0.3f)));
         }
 
         private void TurnRight()
@@ -89,11 +93,11 @@ namespace WordAdventure
 
         private float GetLanePosition()
         {
-            if(currentLane == -1)
+            if (currentLane == -1)
             {
                 return -3.5f;
             }
-            else if(currentLane == 0)
+            else if (currentLane == 0)
             {
                 return 0;
             }
@@ -105,16 +109,16 @@ namespace WordAdventure
 
         private void ChangeLane(int direction)
         {
-            if(direction == -1)
+            if (direction == -1)
             {
-                if(currentLane != -1)
+                if (currentLane != -1)
                 {
                     currentLane -= 1;
                 }
             }
             else
             {
-                if(currentLane != 1)
+                if (currentLane != 1)
                 {
                     currentLane += 1;
                 }
@@ -127,5 +131,26 @@ namespace WordAdventure
             yield return new WaitForSeconds(0.5f);
             TouchController.OnTouchDown += RaiseTouchDownEvent;
         }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Letter l = other.gameObject.GetComponent<Letter>();
+
+            if (l.Lane == currentLane)
+            {
+                GameManager.AddCaughtLetter(l.letter);
+            }
+
+            StartCoroutine(ChangeLetterRoutine(2f, l));
+        }
+
+        private IEnumerator ChangeLetterRoutine(float time, Letter l)
+        {
+            yield return new WaitForSeconds(time);
+            int lane = Random.Range(-1, 2);
+            char c = st[Random.Range(0, st.Length)];
+            l.SetLetter(lane, c.ToString());
+        }
+
     }
 }
